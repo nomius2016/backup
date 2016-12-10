2f4d1bdfd2267f843b130c738f5edd536da2dcae
Tom <yxyy101@example.com>
2016-12-10 18:23:47 +0800

    去掉了H+框架一些用不上的功能

--- application/views/admin/index_main.php
+++ application/views/admin/index_main.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="renderer" content="webkit">
 
-    <title>H+ 后台主题UI框架 - 主页</title>
+    <title>D+业务后台</title>
 
     <!--[if lt IE 9]>
     <meta http-equiv="refresh" content="0;ie.html" />
@@ -111,20 +111,17 @@
             <div class="sidebar-container">
 
                 <ul class="nav nav-tabs navs-3">
-
+					<!--
                     <li class="active">
                         <a data-toggle="tab" href="/static/hplus/#tab-1">
                             <i class="fa fa-gear"></i> 主题
                         </a>
                     </li>
+                    
                     <li class=""><a data-toggle="tab" href="/static/hplus/#tab-2">
                         通知
                     </a>
-                    </li>
-                    <li><a data-toggle="tab" href="/static/hplus/#tab-3">
-                        项目进度
-                    </a>
-                    </li>
+                    </li>-->
                 </ul>
 
                 <div class="tab-content">
@@ -175,35 +172,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="title">皮肤选择</div>
-                            <div class="setings-item default-skin nb">
-                                <span class="skin-name ">
-                         <a href="/static/hplus/#" class="s-skin-0">
-                             默认皮肤
-                         </a>
-                    </span>
-                            </div>
-                            <div class="setings-item blue-skin nb">
-                                <span class="skin-name ">
-                        <a href="/static/hplus/#" class="s-skin-1">
-                            蓝色主题
-                        </a>
-                    </span>
-                            </div>
-                            <div class="setings-item yellow-skin nb">
-                                <span class="skin-name ">
-                        <a href="/static/hplus/#" class="s-skin-3">
-                            黄色/紫色主题
-                        </a>
-                    </span>
-                            </div>
                         </div>
                     </div>
                     <div id="tab-2" class="tab-pane">
 
                         <div class="sidebar-title">
                             <h3> <i class="fa fa-comments-o"></i> 最新通知</h3>
-                            <small><i class="fa fa-tim"></i> 您当前有10条未读信息</small>
+                            <small><i class="fa fa-tim"></i> 您当前有0条未读信息</small>
                         </div>
 
                         <div>
@@ -226,62 +201,7 @@
                                     </div>
                                 </a>
                             </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a2.jpg">
-                                    </div>
-                                    <div class="media-body">
-                                        HCY48之音乐大魔王会员专属皮肤已上线，快来一键换装拥有他，宣告你对华晨宇的爱吧！
-                                        <br>
-                                        <small class="text-muted">昨天 2:45</small>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a3.jpg">
-
-                                        <div class="m-t-xs">
-                                            <i class="fa fa-star text-warning"></i>
-                                            <i class="fa fa-star text-warning"></i>
-                                            <i class="fa fa-star text-warning"></i>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        写的好！与您分享
-                                        <br>
-                                        <small class="text-muted">昨天 1:10</small>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a4.jpg">
-                                    </div>
-
-                                    <div class="media-body">
-                                        国外极限小子的炼成！这还是亲生的吗！！
-                                        <br>
-                                        <small class="text-muted">昨天 8:37</small>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a8.jpg">
-                                    </div>
-                                    <div class="media-body">
-
-                                        一只流浪狗被收留后，为了减轻主人的负担，坚持自己觅食，甚至......有些东西，可能她比我们更懂。
-                                        <br>
-                                        <small class="text-muted">今天 4:21</small>
-                                    </div>
-                                </a>
-                            </div>
+                            
                             <div class="sidebar-message">
                                 <a href="/static/hplus/#">
                                     <div class="pull-left text-center">
@@ -294,127 +214,7 @@
                                     </div>
                                 </a>
                             </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a3.jpg">
-
-                                        <div class="m-t-xs">
-                                            <i class="fa fa-star text-warning"></i>
-                                            <i class="fa fa-star text-warning"></i>
-                                            <i class="fa fa-star text-warning"></i>
-                                        </div>
-                                    </div>
-                                    <div class="media-body">
-                                        最近在补追此剧，特别喜欢这段表白。
-                                        <br>
-                                        <small class="text-muted">昨天 1:10</small>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="sidebar-message">
-                                <a href="/static/hplus/#">
-                                    <div class="pull-left text-center">
-                                        <img alt="image" class="img-circle message-avatar" src="/static/hplus/img/a4.jpg">
-                                    </div>
-                                    <div class="media-body">
-                                        我发起了一个投票 【你认为下午大盘会翻红吗？】
-                                        <br>
-                                        <small class="text-muted">星期一 8:37</small>
-                                    </div>
-                                </a>
-                            </div>
                         </div>
-
-                    </div>
-                    <div id="tab-3" class="tab-pane">
-
-                        <div class="sidebar-title">
-                            <h3> <i class="fa fa-cube"></i> 最新任务</h3>
-                            <small><i class="fa fa-tim"></i> 您当前有14个任务，10个已完成</small>
-                        </div>
-
-                        <ul class="sidebar-list">
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>市场调研</h4> 按要求接收教材；
-
-                                    <div class="small">已完成： 22%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
-                                    </div>
-                                    <div class="small text-muted m-t-xs">项目截止： 4:00 - 2015.10.01</div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>可行性报告研究报上级批准 </h4> 编写目的编写本项目进度报告的目的在于更好的控制软件开发的时间,对团队成员的 开发进度作出一个合理的比对
-
-                                    <div class="small">已完成： 48%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 48%;" class="progress-bar"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>立项阶段</h4> 东风商用车公司 采购综合综合查询分析系统项目进度阶段性报告武汉斯迪克科技有限公司
-
-                                    <div class="small">已完成： 14%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <span class="label label-primary pull-right">NEW</span>
-                                    <h4>设计阶段</h4>
-                                    <!--<div class="small pull-right m-t-xs">9小时以后</div>-->
-                                    项目进度报告(Project Progress Report)
-                                    <div class="small">已完成： 22%</div>
-                                    <div class="small text-muted m-t-xs">项目截止： 4:00 - 2015.10.01</div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>拆迁阶段</h4> 科研项目研究进展报告 项目编号: 项目名称: 项目负责人:
-
-                                    <div class="small">已完成： 22%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 22%;" class="progress-bar progress-bar-warning"></div>
-                                    </div>
-                                    <div class="small text-muted m-t-xs">项目截止： 4:00 - 2015.10.01</div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>建设阶段 </h4> 编写目的编写本项目进度报告的目的在于更好的控制软件开发的时间,对团队成员的 开发进度作出一个合理的比对
-
-                                    <div class="small">已完成： 48%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 48%;" class="progress-bar"></div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/static/hplus/#">
-                                    <div class="small pull-right m-t-xs">9小时以后</div>
-                                    <h4>获证开盘</h4> 编写目的编写本项目进度报告的目的在于更好的控制软件开发的时间,对团队成员的 开发进度作出一个合理的比对
-
-                                    <div class="small">已完成： 14%</div>
-                                    <div class="progress progress-mini">
-                                        <div style="width: 14%;" class="progress-bar progress-bar-info"></div>
-                                    </div>
-                                </a>
-                            </li>
-
-                        </ul>
 
                     </div>
                 </div>
@@ -422,7 +222,7 @@
             </div>
         </div>
         <!--右侧边栏结束-->
-        <!--mini聊天窗口开始-->
+        <!--mini聊天窗口开始
         <div class="small-chat-box fadeInRight animated">
 
             <div class="heading" draggable="true">
@@ -514,7 +314,7 @@
 
             </a>
         </div>
-        <!--mini聊天窗口结束-->
+        mini聊天窗口结束-->
     </div>
 
     <!-- 全局js -->
