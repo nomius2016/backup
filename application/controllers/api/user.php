d3d8a7abac2740c7c846898cf197180588fa6b78
nomius2016 <nomius2016@gmail.com>
2016-12-14 01:04:45 +0800

    用户基本信息接口

--- application/controllers/api/user.php
+++ application/controllers/api/user.php
@@ -27,5 +27,24 @@ class User extends Basecontroller {
 		$this->teamapi($ret);
 	}
 
+	/**
+	 * [basic_info 用户基本信息]
+	 * @return [type] [description]
+	 */
+	public function basic_info(){
+
+		$userinfo = json_decode('{"MainAccountSN":"d3c1ec19-2748-4a03-9f68-7e5379d46007","LanguageCode":"zh-cn","CurrencyID":2,"HandicapID":2,"FirstName":"","MiddleName":"","LastName":"","IDVerifiedNumber":null,"Birthday":null,"Gender":null,"EMail":null,"AreaCode":"86","ContactNumber":"3snd5EsUzaVOqehdl4IUkQ==","CountryID":2,"City":null,"Region":null,"ZipCode":null,"Address":null,"SecurityQuestionID":null,"SecurityAnswer":null,"NewsLetter":1,"Image":null,"PromotionType":1,"PromotionCode":"308","QQAccount":null,"SkypeAccount":null,"PromotionChannel":null,"PromotionExperience":null,"URL":null,"URL_Previous":null,"PromotionID":134496,"Audittype":0,"IsApply":1}');
+
+		$ret = array(
+			'status'=>true,
+			'msg'=>'获取成功', 
+			'code'=>1,
+			'result'=>array(array($userinfo))
+		);
+
+		$this->teamapi($ret);
+
+	}  
+
 }
 
