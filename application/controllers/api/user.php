9f6f35d33ecde1ab1ad121caf62c86086afdba6d
Nomius <nomius1990@gmail.com>
2017-01-02 22:18:10 +0800

    在basic_info 接口 返回安全问题是否设置

--- application/controllers/api/user.php
+++ application/controllers/api/user.php
@@ -72,6 +72,7 @@ class User extends Basecontroller {
 		        'register_time'       => $userinfo['register_time'],
 		        'gender'              => $profile['sex'],
 		        'binded_card_count'   => intval($this->user_bank_card->bindedCardCount($this->user_id)),
+		        'question_setted'     => false
 			);
 
 		$ret = array(
